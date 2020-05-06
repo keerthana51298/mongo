8a16f01a7523a96c3a125cffadc96aca42763d18
keerthana51298 <keerthanaat1998@gmail.com>
2020-05-06 11:55:37 +0530

    hello

---
+++ models/stand_up.js
@@ -0,0 +1,13 @@
+const mongoose=require('mongoose')
+
+
+
+const standupSchema=new mongoose.Schema({
+    StudentName:{type:String},
+    Student_id:{type:String},
+    phone:{type:Number},
+    course:{type:String},
+    Date_of_birth:{type:Date,default:Date.now}
+   
+}) 
+module.exports=mongoose.model('Standup',standupSchema)
